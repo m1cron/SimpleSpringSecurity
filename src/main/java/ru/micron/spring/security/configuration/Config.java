df310aea9f0c0eeffacbf800c4b3bed4493944a3
m1cron <m1cron4900@gmail.com>
2021-01-26 17:31:34 +0300

    added pom.xml, config class; edited .gitignore

---
+++ src/main/java/ru/micron/spring/security/configuration/Config.java
@@ -0,0 +1,23 @@
+package ru.micron.spring.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "ru.micron.spring.security")
+@EnableWebMvc
+public class Config {
+
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
