7385fdd4e53d7c9c5aad84d3d91561d92b4c31f4
m1cron <m1cron4900@gmail.com>
2021-01-26 18:40:23 +0300

    added taglibs int pom.xml

--- src/main/java/ru/micron/spring/security/configuration/SecurityConfig.java
+++ src/main/java/ru/micron/spring/security/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.micron.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,4 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(userBuilder.username("maxim").password("123").roles("HR"))
                 .withUser(userBuilder.username("admin").password("123").roles("MANAGER", "HR"));
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                                .antMatchers("/hr_info").hasRole("HR")
+                                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                        .and().formLogin().permitAll();
+    }
+
 }
